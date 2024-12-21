612eaaf7b4e1b0bc9b155f53753a0bda2ca79067
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-12-21 14:17:37 +0600

    Enhance API structure and authentication middleware: add company, job, and application routes; improve token verification logic

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
-dotenv.config();
+dotenv.config({});
 
 const app = express();
 
@@ -13,21 +16,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
-// cors
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-// api routes
+// api's
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
   connectDB();
+  console.log(`Server running at port ${PORT}`);
 });
--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,26 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = (req, res, next) => {
-  try {
-    const token = req.cookies.token;
-    if (!token) {
-      res.status(401).json({
-        message: "Unauthorized",
-        success: false,
-      });
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authenticated",
+                success: false,
+            })
+        }
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        };
+        req.id = decode.userId;
+        next();
+    } catch (error) {
+        console.log(error);
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decoded) {
-      res.status(401).json({
-        message: "Invalid token",
-        success: false,
-      });
-    }
-    req.id = decoded.userId;
-    next();
-  } catch (error) {
-    console.log(error);
-  }
-};
-
+}
 export default isAuthenticated;
