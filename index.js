384797d0c95439e5bb7b05bea82189f88cee15c0
sanjana <sanjanakazisupti@gmail.com>
2024-12-26 23:32:17 +0600

    supti's commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config({});
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "*",
+  credentials: false,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT;
+
+
+app.get("/", (req, res)=>{
+  return res.json({
+    message: "Hi, I'm from home"
+  });
+})
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
