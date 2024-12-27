d7e8e5982b3599d0e782c50a1c4778c26fbe09cd
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-12-27 11:10:36 +0600

    Refactor authentication middleware and routes for TypeScript compatibility; enhance error handling and improve code formatting

--- backend/middlewares/isAuthenticated.ts
+++ backend/middlewares/isAuthenticated.ts
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = async (req, res, next) => {
+const isAuthenticated = async (req: any, res: any, next: any) => {
   try {
     const token = req.cookies.token;
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
+        success: false,
+      });
+    }
+    if (!process.env.SECRET_KEY) {
+      return res.status(500).json({
+        message: "Internal server error",
         success: false,
       });
     }
@@ -16,7 +22,14 @@ const isAuthenticated = async (req, res, next) => {
         success: false,
       });
     }
-    req.id = decode.userId;
+    if (typeof decode !== "string" && "userId" in decode) {
+      req.headers["userId"] = decode.userId;
+    } else {
+      return res.status(401).json({
+        message: "Invalid token",
+        success: false,
+      });
+    }
     next();
   } catch (error) {
     console.log(error);
